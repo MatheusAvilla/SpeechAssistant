747a898c501900f8a7cd431737cf787a1a05e577
Matheus Avilla Vial <matheus.vial@facens.br>
2021-11-12 16:27:01 -0300

    alterações finalizadas

--- voice.py
+++ voice.py
@@ -1,3 +1,5 @@
+# Realizar um pip install no arquivo PyAudio-0.2.11-cp39-cp39-win_amd64.whl (pip install PyAudio-0.2.11-cp39-cp39-win_amd64.whl)
+
 import speech_recognition as sr
 import webbrowser
 import time
--- weather.py
+++ weather.py
@@ -1,3 +1,5 @@
+import requests
+
 class WeatherService(object):
     API_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=metric"
     API_KEY = "2442280e6f8d5c75eae74e83495073c8"
@@ -9,12 +11,12 @@ class WeatherService(object):
         r = requests.get(WeatherService.API_URL.format(city_name, WeatherService.API_KEY))
         wheather_data = (r.json())
         temp = self._extract_temp(wheather_data)
-        description = self._extract_desc(weather_data)
+        description = self._extract_desc(wheather_data)
         return "Atualmente, em {}, está {} graus com {}".format(city_name, temp, description)
 
     def _extract_temp(self, weatherdata):
         temp = weatherdata['main']['temp']
         return temp
 
-    def _extract_desc(self, weatherdata)
-    return weatherdata['weather'][0]['description']
+    def _extract_desc(self, weatherdata):
+        return weatherdata['weather'][0]['description']
